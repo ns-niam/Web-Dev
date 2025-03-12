beeb5770133073e345f2dd5678494c70a6711049
Md Sha Niamatullah <193333628+ns-niam@users.noreply.github.com>
2025-03-12 19:47:32 +0500

    Create product.ts

---
+++ lab5/online-store/src/app/model /product.ts
@@ -0,0 +1,11 @@
+export interface Product {
+  id: number;
+  title: string;
+  price: number;
+  image: string;
+  rating: number;
+  description: string;
+  link: string;
+  category: string;
+  likes: number;
+}
