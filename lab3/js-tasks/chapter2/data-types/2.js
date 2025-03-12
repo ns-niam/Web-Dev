5b7ba966a6203bfe420c14b6df33f620706e2398
Md Sha Niamatullah <193333628+ns-niam@users.noreply.github.com>
2025-03-12 12:01:27 +0500

    Create 2.js

---
+++ lab3/js-tasks/chapter2/data-types/2.js
@@ -0,0 +1,7 @@
+let name = "Niam";
+
+alert( `hello ${1}` ); //The output is 'hello 1'
+
+alert( `hello ${"name"}` ); // The output is 'hello name'
+
+alert( `hello ${name}` ); // The output is 'hello Niam' 
