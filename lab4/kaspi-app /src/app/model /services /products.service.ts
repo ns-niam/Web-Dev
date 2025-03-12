dfb1e8c51dc3f085a8c88844f7a9c4fd04fc46b0
Md Sha Niamatullah <193333628+ns-niam@users.noreply.github.com>
2025-03-12 12:57:53 +0500

    Update products.service.ts

--- lab4/kaspi-app /src/app/model /services /products.service.ts
+++ lab4/kaspi-app /src/app/model /services /products.service.ts
@@ -7,39 +7,39 @@ import { Product } from '../model/product.model';
 export class ProductsService {
   products: Product[] = [
     {
-      title: 'Versace Eros туалетная вода EDT 100 мл, для мужчин',
+      title: 'Versace Eros Eau de Toilette EDT 100 ml, for men',
       description:
-        'Совершенство мужского тела воплощает идеалы греческой мифологии и классической скульптуры, чья атмосфера сопровождает стиль Дома Versace с момента его основания. Он - Эрос, бог любви, вооруженный луком и стрелами, способный заставить человека полюбить. Versace Eros - аромат для сильного, страстного и уверенного в себе мужчины.',
+        'The perfection of the male body embodies the ideals of Greek mythology and classical sculpture, whose atmosphere has accompanied the style of the House of Versace since its founding. He is Eros, the god of love, armed with a bow and arrows, capable of making a person fall in love. Versace Eros is a fragrance for a strong, passionate, and confident man.',
       rating: 5,
-      price: '39 990',
+      price: '39,990',
       link: 'https://kaspi.kz/shop/p/versace-eros-tualetnaja-voda-edt-100-ml-dlja-muzhchin-17301879/?c=750000000',
       image:
         'https://resources.cdn-kaspi.kz/img/m/p/heb/h0e/63817908092958.jpg?format=gallery-medium',
     },
     {
-      title: 'Apple iPhone 16 Pro Max 256Gb черный',
+      title: 'Apple iPhone 16 Pro Max 256GB Black',
       description:
-        'Флагманский IPhone 16 Pro Max вобрал в себя лучшие черты современного гаджета. Это самое производительное устройство в линейке Apple iPhone с масштабным дисплеем, потрясающей производительностью и невероятной внешней эргономикой. Iphone 16 pro max – настоящий титан в своем семействе. Флагманская модель с тончайшими рамками корпуса обладает эргономичным дизайном, высокой ударо- и влагозащитой. Дополнена передней панелью Ceramic Shield последнего поколения и задней – из особого текстурированного стекла с матовой поверхностью. В серии несколько вариантов смартфонов – на 1Тб, на 256Гб, а также iphone 16 pro max 512GB в черном, титановом, белом цвете и новом нежном оттенке Desert Titanium.',
+        'The flagship iPhone 16 Pro Max incorporates the best features of a modern gadget. It is the most powerful device in the Apple iPhone lineup, featuring a large display, stunning performance, and incredible external ergonomics. The iPhone 16 Pro Max is a true titan in its family. The flagship model with ultra-thin bezels has an ergonomic design, high impact, and water resistance. It is complemented by a next-generation Ceramic Shield front panel and a specially textured glass back panel with a matte finish. The series includes several smartphone options: 1TB, 256GB, and iPhone 16 Pro Max 512GB in black, titanium, white, and the new Desert Titanium shade.',
       rating: 5,
-      price: '651 837',
+      price: '651,837',
       link: 'https://kaspi.kz/shop/p/apple-iphone-16-pro-max-256gb-chernyi-123787551/?c=750000000',
       image:
         'https://resources.cdn-kaspi.kz/img/m/p/hcf/h69/87295489343518.png?format=preview-large',
     },
     {
-      title: 'Apple AirPods 3 with Lightning Charging Case белый',
+      title: 'Apple AirPods 3 with Lightning Charging Case White',
       description:
-        'Динамический драйвер, разработанный Apple, использует специальный усилитель, обеспечивая невероятную детальность звучания. Вы будете чётко слышать весь спектр — от глубоких, насыщенных басов до кристально чистых высоких нот.',
+        'A dynamic driver developed by Apple uses a special amplifier, delivering incredible sound detail. You will hear the entire range clearly—from deep, rich bass to crystal-clear high notes.',
       rating: 5,
       link: 'https://kaspi.kz/shop/p/apple-airpods-3-with-lightning-charging-case-belyi-106667987/?c=750000000',
-      price: '67 800',
+      price: '67,800',
       image:
         'https://resources.cdn-kaspi.kz/img/m/p/h31/hd7/64362668556318.jpg?format=preview-large',
     },
     {
-      title: 'Bonduelle кукуруза сладкая 170 г',
+      title: 'Bonduelle Sweet Corn 170g',
       description:
-        ' Тот самый чудесный вкус, которые многие помнят с детства! Сладкая кукуруза тщательно отбирается, прежде чем попасть в банку, процесс от сбора с грядки до упаковки проходит всего за 4 часа, поэтому на вашем столе всегда самые отборные, сочные и золотистые зерна. Создавайте классические, традиционные блюда с сочной и универсальной кукурузой от Bonduelle.',
+        'That wonderful taste that many remember from childhood! The sweet corn is carefully selected before being canned, with the process from harvest to packaging taking only 4 hours. That’s why your table always has the finest, juiciest, and golden grains. Create classic, traditional dishes with Bonduelle’s juicy and versatile corn.',
       rating: 4,
       link: 'https://kaspi.kz/shop/p/bonduelle-kukuruza-sladkaja-170-g-100213747/?c=750000000',
       price: '610',
@@ -47,64 +47,63 @@ export class ProductsService {
         'https://resources.cdn-kaspi.kz/img/m/p/p77/pd0/23679124.jpeg?format=gallery-medium',
     },
     {
-      title: 'Боксеры белый',
+      title: 'White Boxers',
       description:
-        'Представляем вашему вниманию хлопковые летние детские боксеры от магазина Buyeasy — идеальное нижнее бельё для мальчиков. Эти тонкие боксеры изготовлены из высококачественного хлопка, благодаря которому они приятны на ощупь и позволяют коже дышать.',
-
+        'We present to you cotton summer boys’ boxers from Buyeasy — the perfect underwear for boys. These thin boxers are made of high-quality cotton, making them pleasant to the touch and allowing the skin to breathe.',
       rating: 5,
       link: 'https://kaspi.kz/shop/p/boksery-147234-belyi-98-104-121129825/?c=750000000',
-      price: '2 499',
+      price: '2,499',
       image:
         'https://resources.cdn-kaspi.kz/img/m/p/h32/h75/86732733087774.jpg?format=gallery-medium',
     },
     {
-      title: 'Яшкино пряники шоколадные 350 г',
+      title: 'Yashkino Chocolate Gingerbread 350g',
       description:
-        'Пряники по классической рецептуре из заварного теста с добавлением какао, покрытые сахарной глазурью и глазированным дном.',
+        'Classic gingerbread made from scalded dough with cocoa, covered with sugar glaze and a chocolate-glazed bottom.',
       rating: 5,
       link: 'https://kaspi.kz/shop/p/trapeza-prjaniki-russkie-vanil-nye-350-g-102903335/?c=750000000',
-      price: '699 ',
+      price: '699',
       image:
         'https://resources.cdn-kaspi.kz/img/m/p/h97/hd9/68721829249054.jpg?format=gallery-medium',
     },
     {
-      title: 'Цветные карандаши Derwent 32891 форма шестигранная 72 шт',
+      title: 'Derwent 32891 Hexagonal Colored Pencils, 72 pcs',
       description:
-        'Подарочный набор DERWENT WATERCOLOUR содержит 72 высококачественных акварельных карандаша. Карандаши находятся в красивой деревянной коробке, имеющей логотип фирмы и два небольших замка. Основой для грифеля послужили высококачественные цветные пигменты и белая глина, отличающаяся особой мягкостью и чистотой. Грифель не размазывается и не крошится, тона легко смешиваются между собой. Поразительно яркие цвета и красивые гладкие штрихи придадут рисунку максимальную достоверность. Можно использовать сухими, как обычные карандаши. Чтобы создать потрясающие эффекты, которые ожидаются от акварели в форме карандаша, нужно всего лишь немного воды.',
+        'The DERWENT WATERCOLOUR gift set includes 72 high-quality watercolor pencils. The pencils come in a beautiful wooden box with the company’s logo and two small locks. The core is made of high-quality colored pigments and white clay, known for its special softness and purity. The core does not smear or crumble, and the tones blend easily. Strikingly bright colors and smooth, beautiful strokes add maximum realism to the drawing. They can be used dry, like regular pencils. To achieve the stunning effects expected from watercolor pencils, simply add a little water.',
       rating: 5,
       link: 'https://kaspi.kz/shop/p/derwent-32891-forma-shestigrannaja-72-sht-120836981/?c=750000000',
-      price: '168 500 ',
+      price: '168,500',
       image:
         'https://resources.cdn-kaspi.kz/img/m/p/hd0/h7c/86390559932446.jpg?format=gallery-medium',
     },
     {
       title:
-        'Ноутбук Apple MacBook Pro 16 2023 16.2" / 48 Гб / SSD 1024 Гб / macOS / MUW63',
+        'Apple MacBook Pro 16 2023 16.2" / 48GB / SSD 1024GB / macOS / MUW63',
       description:
-        'Apple MacBook Pro 16 M3 Max 2023 - идеально подходит для профессионалов в любых областях и сферах. Он сделает вашу работу более продуктивной за счет повышенной производительности, времени автономной работы и возможностей подключения.',
+        'The Apple MacBook Pro 16 M3 Max 2023 is perfect for professionals in any field. It enhances productivity with increased performance, battery life, and connectivity options.',
       rating: 5,
       link: 'https://kaspi.kz/shop/p/apple-macbook-pro-16-2023-16-2-48-gb-ssd-1024-gb-macos-muw63-114863237/?c=750000000',
-      price: '1 971 998 ',
+      price: '1,971,998',
       image:
         'https://resources.cdn-kaspi.kz/img/m/p/hf8/h2e/84577772339230.jpg?format=gallery-medium',
     },
     {
-      title: 'Дыня ЖАНСАЯ',
+      title: 'Jansaya Melon',
       description:
-        'Подарочный набор DERWENT WATERCOLOUR содержит 72 высококачественных акварельных карандаша. Карандаши находятся в красивой деревянной коробке, имеющей логотип фирмы и два небольших замка. Основой для грифеля послужили высококачественные цветные пигменты и белая глина, отличающаяся особой мягкостью и чистотой. Грифель не размазывается и не крошится, тона легко смешиваются между собой. Поразительно яркие цвета и красивые гладкие штрихи придадут рисунку максимальную достоверность. Можно использовать сухими, как обычные карандаши. Чтобы создать потрясающие эффекты, которые ожидаются от акварели в форме карандаша, нужно всего лишь немного воды.',
+        'The DERWENT WATERCOLOUR gift set includes 72 high-quality watercolor pencils. The pencils come in a beautiful wooden box with the company’s logo and two small locks. The core is made of high-quality colored pigments and white clay, known for its special softness and purity. The core does not smear or crumble, and the tones blend easily. Strikingly bright colors and smooth, beautiful strokes add maximum realism to the drawing. They can be used dry, like regular pencils. To achieve the stunning effects expected from watercolor pencils, simply add a little water.',
       rating: 5,
       link: 'https://kaspi.kz/shop/p/dynja-zhansaja-116786805/?c=750000000',
-      price: '500 ',
+      price: '500',
       image:
         'https://resources.cdn-kaspi.kz/img/m/p/he4/hb5/85224578023454.jpg?format=gallery-medium',
     },
     {
-      title: 'Рахат Kazakhstan шоколадная плитка темный 100 г',
+      title: 'Rahat Kazakhstan Dark Chocolate Bar 100g',
       description:
-        'Особенно вкусный молочный шоколад Казахстанский с ванильным ароматом изготовлен из отборных какао-бобов по классической технологии на шоколадной фабрике Рахат. Натуральный шоколад зарядит Вас энергией и подарит радость на весь день.',
+        'Especially delicious Kazakh milk chocolate with a vanilla aroma, made from selected cocoa beans using a classic technology at the Rahat chocolate factory. Natural chocolate will energize you and bring joy all day long.',
       rating: 5,
       link: 'https://kaspi.kz/shop/p/rahat-kazakhstan-shokoladnaja-plitka-temnyi-100-g-100221859/?c=750000000',
-      price: '735 ',
+      price: '735',
       image:
         'https://resources.cdn-kaspi.kz/img/m/p/h21/h90/70365637279774.jpg?format=gallery-medium',
     },
