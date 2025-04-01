2ee5846d0ee3a99159083f2e4ae97be415c3536f
Md Sha Niamatullah <193333628+ns-niam@users.noreply.github.com>
2025-04-01 10:32:19 +0500

    Create urls.py

---
+++ lab8/shopback/api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("products", views.product_list),
+    path("products/<int:id>", views.product_by_id),
+    path("categories", views.categories),
+    path("categories/<int:id>", views.category_by_id),
+    path("categories/<int:id>/products", views.products_by_category),
+]
+    
