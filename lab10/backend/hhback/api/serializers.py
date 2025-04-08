3e03b9d8870e9f95882559b2abfa267a494700fe
Md Sha Niamatullah <193333628+ns-niam@users.noreply.github.com>
2025-04-09 03:03:42 +0500

    Create serializers.py

---
+++ lab10/backend/hhback/api/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import Company, Vacancy
+
+class CompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Company
+        fields = '__all__'
+    
+class VacancySerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    name = serializers.CharField(max_length=100)
+    description = serializers.CharField(allow_blank=True, required=False)
+    salary = serializers.FloatField()
+    company = serializers.PrimaryKeyRelatedField(queryset=Company.objects.all())
+    company_name = serializers.CharField(source='company.name', read_only=True)
+    
